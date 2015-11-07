1e553709770a473a4df8f07dd1ba35ba2d5c20d2
Bidouilleur Numerique <contact@bidouilleur-numerique.com>
2015-11-07 10:53:17 +0000

    Refining music cover thumb

--- app/models/music.rb
+++ app/models/music.rb
@@ -3,7 +3,7 @@ class Music < ActiveRecord::Base
   friendly_id :slug_candidates, use: [:slugged, :finders]
   
   has_many :tracks
-  has_attached_file :cover, styles: { thumb: "300x300>" }
+  has_attached_file :cover, styles: { thumb: "300x300#" }
   validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
   accepts_nested_attributes_for :tracks, allow_destroy: true
   
